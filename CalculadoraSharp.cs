6069aad520210b9e428c1f99339b2bfc4ffa58b3
root <root@localhost.localdomain>
2021-08-11 00:56:10 -0300

    Atualização.

--- CalculadoraSharp.cs
+++ CalculadoraSharp.cs
@@ -1,21 +1,110 @@
 using System;
 
-namespace ConsoleApp2
+namespace Calculadora
 {
     class Program
     {
         static void Main(string[] args)
         {
-            double n1 = 0.0;
-            double n2 = 0.0;
+            byte escolha = 0;
+
+            do
+            {
+                Console.WriteLine("1 ... Somar.");
+                Console.WriteLine("2 ... subtrair.");
+                Console.WriteLine("3 ... multiplicar.");
+                Console.WriteLine("4 ... dividir.");
+                Console.WriteLine("0 ... Sair.");
+                Console.WriteLine("Escolha -> ");
+                
+                escolha = byte.Parse(Console.ReadLine());
+
             
-            Console.WriteLine("Digite o primeiro número: ");
-            n1 = double.Parse(Console.ReadLine());
-            
-            Console.WriteLine("Digite o segundo número: ");
-            n2 = double.Parse(Console.ReadLine());
-            
-            Console.WriteLine("Resultado: " + (n1 + n2));
+                    switch (escolha)
+                    {
+                        case 1:
+                            {
+                                Console.WriteLine("Digite o número 1: ");
+                                double n1 = double.Parse(Console.ReadLine());
+
+                                Console.WriteLine("Digite o número 2: ");
+                                double n2 = double.Parse(Console.ReadLine());
+
+                                Console.WriteLine("Resultado: " + somar(n1, n2));
+                                break;
+                            }
+
+                        case 2:
+                            {
+                                Console.WriteLine("Digite o número 1: ");
+                                double n1 = double.Parse(Console.ReadLine());
+
+                                Console.WriteLine("Digite o número 2: ");
+                                double n2 = double.Parse(Console.ReadLine());
+
+                                Console.WriteLine("Resultado: " + subtracao(n1, n2));
+                                break;
+                            }
+
+                        case 3:
+                            {
+                                Console.WriteLine("Digite o número 1: ");
+                                double n1 = double.Parse(Console.ReadLine());
+
+                                Console.WriteLine("Digite o número 2: ");
+                                double n2 = double.Parse(Console.ReadLine());
+
+                                Console.WriteLine("Resultado: " + multiplicacao(n1, n2));
+                                break;
+                            }
+
+                        case 4:
+                            {
+                                Console.WriteLine("Digite o número 1: ");
+                                double n1 = double.Parse(Console.ReadLine());
+
+                                Console.WriteLine("Digite o número 2: ");
+                                double n2 = double.Parse(Console.ReadLine());
+
+                                Console.WriteLine("Resultado: " + divisao(n1, n2));
+                                break;
+                            }
+
+                    case 0:
+                        {
+                            Console.WriteLine("Saindo!");
+                            break;
+                        }
+
+                    default:
+                            {
+                                Console.WriteLine("Não reconheci sua escolha, tente novamente!");
+                                break;
+                            }
+
+                } 
+
+            } while (escolha != 0);
+        }
+
+        static double somar(double n1, double n2)
+        {
+            return n1 + n2;
+        }
+
+        static double subtracao(double n1, double n2)
+        {
+            return n1 - n2;
+        }
+
+        static double multiplicacao(double n1, double n2)
+        {
+            return n1 * n2;
+        }
+
+        static double divisao(double n1, double n2)
+        {
+            return n1 / n2;
         }
     }
 }
